64d8fb0c8b944fe9f37d963302205df6ee13908f
ishikanimmagadda <ishikan84@gmail.com>
2024-06-19 15:02:17 -0500

    grade dictionary, writeToFile, readFile implement

--- GradeManagement.py
+++ GradeManagement.py
@@ -1,45 +1,81 @@
 import csv
 
-#fields = ['Name', 'Grade']
-
-#file1 = open("gradefile.txt", "r")
 class Student: 
     def __init__(self, name, grade): 
-        self.name = []
-        self.grade = []
+        self.name = name
+        self.grade = {}
 
-    def setGrade(self, grade): 
-        self.grade.append(grade)
+    def setGrade(self, key, value): 
+        self.grade[key] = value
     
     def setName(self, name): 
-        self.name.append(name)
+        self.name = name
     
-    def removeGrade(self, grade): 
-        self.grade.remove(grade)
+    def removeGrade(self, key): 
+        if key in self.grade: 
+            del self.grade[key]
     
     def displayGrades(self): 
-        for name in self.name: 
-            print("Name: " + name + ", Grades: " + str(self.grade))
+        print("Name: " + self.name + ", Grades: " + str(self.grade))
     
     def writeToFile(self, filename):
-        file = open(filename, 'w')
-        for name in self.name: 
-            file.writelines("Name: " + name + ", Grades: " + str(self.grade) + "\n")
+        file = open(filename, 'a')
+        file.writelines("Name: " + self.name + ", Grades: " + str(self.grade) + "\n")
         file.close()
     
+    def readFile(self, filename): 
+        file = open(filename, 'r')
+        content = file.readlines()
+        print ("Readlines: " + str(content))
+        file.close()
+        
 
-Student1 =  Student([],[])
+    
+    
+#for every new run overwrites old info from file 
+file = open("gradefile.txt", 'w')
+
+#declaring class variable 
+Student1 =  Student("Ishika",{})
 Student1.setName("Ishika")
-Student1.setGrade(80)
-Student1.setGrade(98)
+
+#user input grade
+Student1.key = input("Enter the Subject: ")
+Student1.value = input("Enter a Grade: ")
+Student1.setGrade(Student1.key, Student1.value)
+
+Student1.setGrade("Math",90)
 Student1.displayGrades()
+
+#testing remove 
+Student1.removeGrade(Student1.key)
+Student1.displayGrades()
+
+#writing grades excluding removed grade
 Student1.writeToFile("gradefile.txt")
 
-Student2 =  Student([],[])
+Student1.readFile("gradefile.txt")
+
+
+
+
+Student2 =  Student("Potter",{})
 Student2.setName("Potter")
-Student2.setGrade(30)
-Student2.setGrade(50)
+
+#user input grades 
+Student2.key = input("Enter the Subject: ")
+Student2.value = input("Enter a Grade: ")
+Student2.setGrade(Student2.key, Student2.value)
+
+Student2.setGrade("Science", 87)
 Student2.displayGrades()
+
+#removing Science 
+Student2.removeGrade(key = "Science")
+Student2.displayGrades()
+
 Student2.writeToFile("gradefile.txt")
+
+Student2.readFile("gradefile.txt")
 
 
